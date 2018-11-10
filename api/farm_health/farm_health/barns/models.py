d09667af93aa4c3ad8b4d8cae3c0c63958c3c2dc
jjlendaya <jjlendaya@gmail.com>
2018-11-11 00:00:00 +0800

    Finalize models for Barn, Sensor, and SensorDataPoint

--- api/farm_health/farm_health/barns/models.py
+++ api/farm_health/farm_health/barns/models.py
@@ -11,12 +11,20 @@ class Barn(TimeStampedModel):
         return self.reference_id
 
 
-class SensorDataPoint(TimeStampedModel):
+class Sensor(TimeStampedModel):
+    reference_id = models.UUIDField(default=uuid.uuid4, unique=True)
     barn = models.ForeignKey(Barn, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.reference_id
+
+
+class SensorDataPoint(TimeStampedModel):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
     humidity = models.DecimalField(decimal_places=4, max_digits=12, null=True, blank=True)
     temperature = models.DecimalField(decimal_places=4, max_digits=12, null=True, blank=True)
     air_quality = models.DecimalField(decimal_places=4, max_digits=12, null=True, blank=True)
     image_feed = models.ImageField(upload_to='uploads/feed/')
 
     def __str__(self):
-        return '{}: {}'.format(self.barn, self.created)
+        return '{} - {} - {}'.format(self.sensor.barn, self.sensor, self.created)
