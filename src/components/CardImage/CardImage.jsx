6870b336538f03ec1ab2edcc8345d123afccd2c3
Agnes Jardeleza <paraholic.ablaze@gmail.com>
2018-11-10 21:19:16 +0800

    Initial commit

---
+++ src/components/CardImage/CardImage.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const cardImageStyles = {
+  card: {
+    maxWidth: 445
+  },
+  media: {
+    // ⚠️ object-fit is not supported by IE 11.
+    objectFit: "cover"
+  }
+};
+
+const CardImage = ({ classes, title }) => {
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          className={classes.media}
+          height="200"
+          image="https://aussiepigfarmers.com.au/wp-content/uploads/2015/11/content-relaxed-animals-in-barn-rared-environment1.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h4">
+            {title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+CardImage.propTypes = {
+  classes: PropTypes.object.isRequired,
+  title: PropTypes.string.isRequired
+};
+
+export default withStyles(cardImageStyles)(CardImage);
