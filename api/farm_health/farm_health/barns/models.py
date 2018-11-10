6e134f4d51059d911fb1ca8c65fb3379495ec8a2
jjlendaya <jjlendaya@gmail.com>
2018-11-10 23:37:11 +0800

    Initial commit of barn app with models

---
+++ api/farm_health/farm_health/barns/models.py
@@ -0,0 +1,23 @@
+import uuid
+
+from django.db import models
+from model_utils.models import TimeStampedModel
+from model_utils import Choices
+
+
+class Barn(TimeStampedModel):
+    reference_id = models.UUIDField(default=uuid.uuid4, unique=True)
+
+    def __str__(self):
+        return self.reference_id
+
+
+class SensorDataPoint(TimeStampedModel):
+    barn = models.ForeignKey(Barn, on_delete=models.CASCADE)
+    humidity = models.DecimalField(decimal_places=4, max_digits=12, null=True, blank=True)
+    temperature = models.DecimalField(decimal_places=4, max_digits=12, null=True, blank=True)
+    air_quality = models.DecimalField(decimal_places=4, max_digits=12, null=True, blank=True)
+    image_feed = models.ImageField(upload_to='uploads/feed/')
+
+    def __str__(self):
+        return '{}: {}'.format(self.barn, self.created)
